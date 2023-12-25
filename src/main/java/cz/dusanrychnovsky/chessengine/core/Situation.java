1cead21a6cb494e5dce15f37342042ed31e0f5da
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2023-12-25 18:54:11 +0100

    implement Situation#getInitial

--- src/main/java/cz/dusanrychnovsky/chessengine/core/Situation.java
+++ src/main/java/cz/dusanrychnovsky/chessengine/core/Situation.java
@@ -1,8 +1,14 @@
 package cz.dusanrychnovsky.chessengine.core;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import static cz.dusanrychnovsky.chessengine.core.Color.*;
+import static cz.dusanrychnovsky.chessengine.core.PieceType.*;
+import static cz.dusanrychnovsky.chessengine.core.Position.*;
+import static cz.dusanrychnovsky.chessengine.core.Row.R2;
+import static cz.dusanrychnovsky.chessengine.core.Row.R7;
 import static cz.dusanrychnovsky.chessengine.util.MapExtensions.get;
 
 public class Situation {
@@ -15,11 +21,47 @@ public class Situation {
     this.currentPlayer = currentPlayer;
   }
 
+  public static Situation getInitial() {
+    var pieces = new HashMap<Position, Piece>();
+
+    // white
+    pieces.put(A1, new Piece(WHITE, ROOK));
+    pieces.put(B1, new Piece(WHITE, KNIGHT));
+    pieces.put(C1, new Piece(WHITE, BISHOP));
+    pieces.put(D1, new Piece(WHITE, QUEEN));
+    pieces.put(E1, new Piece(WHITE, KING));
+    pieces.put(F1, new Piece(WHITE, BISHOP));
+    pieces.put(G1, new Piece(WHITE, KNIGHT));
+    pieces.put(H1, new Piece(WHITE, ROOK));
+
+    // black
+    pieces.put(A8, new Piece(BLACK, ROOK));
+    pieces.put(B8, new Piece(BLACK, KNIGHT));
+    pieces.put(C8, new Piece(BLACK, BISHOP));
+    pieces.put(D8, new Piece(BLACK, QUEEN));
+    pieces.put(E8, new Piece(BLACK, KING));
+    pieces.put(F8, new Piece(BLACK, BISHOP));
+    pieces.put(G8, new Piece(BLACK, KNIGHT));
+    pieces.put(H8, new Piece(BLACK, ROOK));
+
+    // pawns
+    for (var col : Column.values()) {
+      pieces.put(Position.get(col, R2), new Piece(WHITE, PAWN));
+      pieces.put(Position.get(col, R7), new Piece(BLACK, PAWN));
+    }
+
+    return new Situation(WHITE, pieces);
+  }
+
   /**
    * @return the piece which currently occupies the given position on the
    * board, if any
    */
   public Optional<Piece> getPieceAt(Position position) {
     return get(pieces, position);
+  }
+
+  public Color getCurrentPlayer() {
+    return currentPlayer;
   }
 }
--- src/test/java/cz/dusanrychnovsky/chessengine/core/SituationTests.java
+++ src/test/java/cz/dusanrychnovsky/chessengine/core/SituationTests.java
@@ -9,7 +9,10 @@ import java.util.Optional;
 import static cz.dusanrychnovsky.chessengine.core.Color.*;
 import static cz.dusanrychnovsky.chessengine.core.PieceType.*;
 import static cz.dusanrychnovsky.chessengine.core.Position.*;
+import static cz.dusanrychnovsky.chessengine.core.Row.*;
+import static cz.dusanrychnovsky.chessengine.core.Column.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SituationTests {
 
@@ -32,5 +35,43 @@ public class SituationTests {
       Optional.empty(),
       new Situation(WHITE, new HashMap<>())
         .getPieceAt(D4));
+  }
+
+  @Test
+  public void getInitialReturnsInitialChessSituation() {
+    var situation = Situation.getInitial();
+    // color
+    assertEquals(WHITE, situation.getCurrentPlayer());
+    // pawns
+    for (var col : Column.values()) {
+      assertPieceAt(situation, Position.get(col, R2), WHITE, PAWN);
+      assertPieceAt(situation, Position.get(col, R7), BLACK, PAWN);
+    }
+    // white
+    assertPieceAt(situation, Position.get(CA, R1), WHITE, ROOK);
+    assertPieceAt(situation, Position.get(CB, R1), WHITE, KNIGHT);
+    assertPieceAt(situation, Position.get(CC, R1), WHITE, BISHOP);
+    assertPieceAt(situation, Position.get(CD, R1), WHITE, QUEEN);
+    assertPieceAt(situation, Position.get(CE, R1), WHITE, KING);
+    assertPieceAt(situation, Position.get(CF, R1), WHITE, BISHOP);
+    assertPieceAt(situation, Position.get(CG, R1), WHITE, KNIGHT);
+    assertPieceAt(situation, Position.get(CH, R1), WHITE, ROOK);
+    // black
+    assertPieceAt(situation, Position.get(CA, R8), BLACK, ROOK);
+    assertPieceAt(situation, Position.get(CB, R8), BLACK, KNIGHT);
+    assertPieceAt(situation, Position.get(CC, R8), BLACK, BISHOP);
+    assertPieceAt(situation, Position.get(CD, R8), BLACK, QUEEN);
+    assertPieceAt(situation, Position.get(CE, R8), BLACK, KING);
+    assertPieceAt(situation, Position.get(CF, R8), BLACK, BISHOP);
+    assertPieceAt(situation, Position.get(CG, R8), BLACK, KNIGHT);
+    assertPieceAt(situation, Position.get(CH, R8), BLACK, ROOK);
+  }
+
+  private void assertPieceAt(Situation situation, Position position, Color color, PieceType pieceType) {
+    var piece = situation.getPieceAt(position);
+    assertTrue(piece.isPresent());
+    assertEquals(
+      new Piece(color, pieceType),
+      piece.get());
   }
 }
