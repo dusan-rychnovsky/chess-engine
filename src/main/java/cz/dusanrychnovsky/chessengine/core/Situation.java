9bd5a9ed7ecf865e3c0877c88ca8991a7ed7dbe7
dusan-rychnovsky <dusan.rychnovsky@gmail.com>
2024-06-28 18:10:55 +0200

    sonar java:S1128

--- src/main/java/cz/dusanrychnovsky/chessengine/core/Situation.java
+++ src/main/java/cz/dusanrychnovsky/chessengine/core/Situation.java
@@ -1,7 +1,6 @@
 package cz.dusanrychnovsky.chessengine.core;
 
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static cz.dusanrychnovsky.chessengine.core.Color.*;
