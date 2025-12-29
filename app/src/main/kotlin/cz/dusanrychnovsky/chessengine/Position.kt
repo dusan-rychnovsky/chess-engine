0185fccb7f0eaf0919c2fc8dda3845be527a6414
Dušan Rychnovský <dusan.rychnovsky@gmail.com>
2025-12-29 15:39:03 +0100

    Implement Position.isValid(Move)

--- app/src/main/kotlin/cz/dusanrychnovsky/chessengine/Position.kt
+++ app/src/main/kotlin/cz/dusanrychnovsky/chessengine/Position.kt
@@ -2,12 +2,22 @@ package cz.dusanrychnovsky.chessengine
 
 data class Position(val pieces: Map<Square, Piece>) {
     fun isValid(move: Move): Boolean {
-        val piece = pieces[move.from] ?: return false
-        if (!piece.type.moves(move.from).contains(move)) {
+        val fromPiece = pieces[move.from] ?: return false
+        if (!fromPiece.type.moves(move.from).contains(move)) {
             return false
         }
 
-        // TODO
+        for (through in move.through) {
+            if (pieces.containsKey(through)) {
+                return false
+            }
+        }
+
+        val toPiece = pieces[move.to]
+        if (toPiece != null && toPiece.color == fromPiece.color) {
+            return false
+        }
+        
         return true
     }
 }
--- app/src/test/kotlin/cz/dusanrychnovsky/chessengine/PositionTest.kt
+++ app/src/test/kotlin/cz/dusanrychnovsky/chessengine/PositionTest.kt
@@ -11,7 +11,9 @@ class PositionTest {
 
     val WHITE_KING = Piece(WHITE, KING)
     val WHITE_ROOK = Piece(WHITE, ROOK)
+    val WHITE_KNIGHT = Piece(WHITE, KNIGHT)
     val BLACK_KING = Piece(BLACK, KING)
+    val BLACK_BISHOP = Piece(BLACK, BISHOP)
 
     @Test
     fun isValid_noPieceAtSource_returnsFalse() {
@@ -44,5 +46,57 @@ class PositionTest {
         assertTrue(position.isValid(Move(F5, E4, emptyList())))
         assertTrue(position.isValid(Move(F5, G6, emptyList())))
         assertFalse(position.isValid(Move(F5, F7, listOf(F6))))
+    }
+
+    @Test
+    fun isValid_piecesCantJumpOverOtherPieces() {
+        val position = Position(
+            mapOf(
+                B3 to WHITE_KING,
+                C2 to WHITE_ROOK,
+                E1 to BLACK_KING,
+                E2 to BLACK_BISHOP,
+            )
+        )
+        assertFalse(position.isValid(Move(C2, F2, listOf(D2, E2))))
+    }
+
+    @Test
+    fun isValid_knightsCanJumpOverOtherPieces() {
+        val position = Position(
+            mapOf(
+                B3 to WHITE_KING,
+                C2 to WHITE_KNIGHT,
+                E2 to BLACK_KING,
+                D3 to BLACK_BISHOP,
+            )
+        )
+        assertTrue(position.isValid(Move(C2, E3, emptyList()    )))
+    }
+
+    @Test
+    fun isValid_piecesCanCaptureOpponentsPieces() {
+        val position = Position(
+            mapOf(
+                B3 to WHITE_KING,
+                C2 to WHITE_ROOK,
+                F5 to BLACK_KING,
+                E2 to BLACK_BISHOP,
+            )
+        )
+        assertTrue(position.isValid(Move(C2, E2, listOf(D2))))
+    }
+
+    @Test
+    fun isValid_piecesCannotCaptureOwnPieces() {
+        val position = Position(
+            mapOf(
+                B3 to WHITE_KING,
+                C2 to WHITE_ROOK,
+                E2 to WHITE_ROOK,
+                F5 to BLACK_KING,
+            )
+        )
+        assertFalse(position.isValid(Move(C2, E2, listOf(D2))))
     }
 }
